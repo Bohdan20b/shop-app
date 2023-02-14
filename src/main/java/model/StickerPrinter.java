2b95df7f115ce59ff6367cfbebe6519e325a14b1
Bohdan Senyk <senyk2001@gmail.com>
2023-02-14 13:21:14 +0200

    reworked adding logic

--- src/main/java/model/StickerPrinter.java
+++ src/main/java/model/StickerPrinter.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class StickerPrinter {
     private static final String INVALID_PRODUCT_NAME = "INVALID_PRODUCT_NAME";
@@ -17,6 +18,7 @@ public class StickerPrinter {
         stringBuilder.append(WHITESPACE);
         if (validatePrice(price)) {
             BigDecimal finalPrice = price.multiply(BigDecimal.valueOf(tax)).add(price);
+            finalPrice = finalPrice.setScale(2, RoundingMode.CEILING);
             stringBuilder.append(finalPrice);
         } else {
             stringBuilder.append(INVALID_PRICE);
